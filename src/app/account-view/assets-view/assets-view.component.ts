fdc846a18543f27ba82de957a4c5871a7029c58f
Vladislav Kudrin <vladislavkudrin@gmail.com>
2022-12-10 20:19:23 +0000

    Show assets in account

--- src/app/account-view/assets-view/assets-view.component.ts
+++ src/app/account-view/assets-view/assets-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardanoWalletBalance } from 'src/app/shared/models/wallet.model';
+import { WalletService } from 'src/app/shared/services/wallet.service';
 
 @Component({
   selector: 'app-assets-view',
@@ -6,14 +8,17 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./assets-view.component.scss']
 })
 export class AssetsViewComponent implements OnInit{
- @Input() utxos;
+
+  public multiassets: any = [];
 
   public constructor(
-
+    private walletService: WalletService,
   ){}
 
   public ngOnInit(): void {
-    console.log(this.utxos)
+    this.walletService.walletBalanceSubject.subscribe((balance: CardanoWalletBalance) => {
+      this.multiassets = balance.multiassets;
+    });
   }
 
 }
